c8538e594724c99628cd596dcfb683ac055bc513
onanuviie <nuviesilvet@gmail.com>
2023-03-03 17:26:48 +0100

    made the game functional

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const tileDisplay = document.querySelector('.tile-container')
 const keyboard = document.querySelector('.key-container')
+const messageDisplay = document.querySelector('.message-container')
 
+const wordle = 'SUPER'
 const keys = [
     'Q',
     'W',
@@ -41,6 +43,10 @@ const guessRows = [
     ['', '', '', '', '']
 ]
 
+let currentRow = 0
+let currentTile = 0
+let isGameOver = false
+
 guessRows.forEach((guessRow, guessRowIndex) => {
     const rowElement = document.createElement('div')
     rowElement.setAttribute('id', 'guessRow-' + guessRowIndex)
@@ -57,16 +63,83 @@ guessRows.forEach((guessRow, guessRowIndex) => {
     tileDisplay.append(rowElement)
 })
 
-
-const handleClick = () => {
-    console.log("clicked")
-}
-
-
 keys.forEach(key => {
     const buttonElement = document.createElement('button')
     buttonElement.textContent = key
     buttonElement.setAttribute('id', key)
-    buttonElement.addEventListener('click', handleClick)
+    buttonElement.addEventListener('click', () => handleClick(key))
     keyboard.append(buttonElement)
 })
+
+const handleClick = (key) => {
+    console.log("clicked", key)
+
+    if (key === '«') {
+        deleteLetter()
+        console.log('guessRows',guessRows)
+        return
+    }
+
+    if (key === 'ENTER') {
+        checkRow()
+        console.log('guessRows',guessRows)
+        return
+    }
+
+    addLetter(key)
+    console.log('guessRows',guessRows)
+}
+
+const addLetter = (letter) => {
+    if (currentTile < 5 && currentRow < 6) {
+        const tile = document.getElementById('guessRow-' + currentRow + 'tile-' + currentTile)
+        tile.textContent = letter
+        guessRows[currentRow][currentTile] = letter
+        tile.setAttribute('data', letter)
+        currentTile++
+    }
+}
+
+const deleteLetter = () => {
+    if (currentTile > 0) {
+        currentTile--
+        const tile = document.getElementById('guessRow-' + currentRow + 'tile-' + currentTile)
+        tile.textContent= ''
+        guessRows[currentRow][currentTile] = ''
+        tile.setAttribute('data', '')
+    }
+}
+
+const checkRow = () => {
+    const guess = guessRows[currentRow].join('')
+
+
+    if (currentTile > 4) {
+         
+         console.log(`my guess is ${guess} and wordle is ${wordle}`)
+         if (wordle == guess){
+            showMessage('Magnificent!')
+            isGameOver = true
+            return
+         }
+         else {
+            if (currentRow >= 5) {
+                isGameOver = true
+                showMessage('Game Over')
+                return
+            }
+            if (currentRow < 5) {
+                currentRow++
+                currentTile = 0
+            }
+         }
+    }
+}
+
+const showMessage = (message) => {
+    const messageElement = document.createElement('p')
+    messageElement.textContent = message
+    messageDisplay.append(messageElement)
+
+    setTimeout(() => messageDisplay.removeChild(messageElement), 2500)
+}
